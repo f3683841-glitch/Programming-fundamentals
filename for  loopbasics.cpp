4f349b8a7f1743c8e163564546df48f29c1e9669
f3683841-glitch <f3683841@gmail.com>
2026-01-20 16:31:08 +0500

    Add files via upload

---
+++ for  loopbasics.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+for(int i=1;i<=10;i++){
+    cout<<i<<endl;
+}
+{
+
+int n;
+cout<<" enter n :";
+cin>>n;
+for(int i=n;i<=n*10;i=i+n){
+    cout<<i<<endl;
+}
+{
+    int n;
+cout<<" enter n :";
+cin>>n;
+int a=4;
+for(int i=1;i<=n;i++){
+    cout<<a<<endl;
+    a=a+3;
+}
+
+
+
+
+}
+
+}
+
+
+
+    return 0;
+}
